6ddde87d22c9519cc3d04810944a76eec15c3280
Aung Pyae Phyo <aungpyaephyo24@gmail.com>
2017-08-11 10:55:48 +0630

    In-code One.

---
+++ app/src/main/java/xyz/aungpyaephyo/padc/rxjava/RxJavaApp.java
@@ -0,0 +1,17 @@
+package xyz.aungpyaephyo.padc.rxjava;
+
+import android.app.Application;
+
+/**
+ * Created by aung on 8/11/17.
+ */
+
+public class RxJavaApp extends Application {
+
+    public static final String TAG = "RxJavaApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/xyz/aungpyaephyo/padc/rxjava/activities/MainActivity.java
@@ -0,0 +1,99 @@
+package xyz.aungpyaephyo.padc.rxjava.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import xyz.aungpyaephyo.padc.rxjava.R;
+import xyz.aungpyaephyo.padc.rxjava.RxJavaApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.tv_text)
+    TextView tvText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this, this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.fab)
+    public void onTapFab(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    @OnClick(R.id.btn_in_code_one)
+    public void onTapBtnInCodeOne(View view) {
+        tvText.setText("");
+        helloRxJava("the", "real", "PADC", ":", "Android", "Developer", "Course");
+    }
+
+    private void helloRxJava(String... names) {
+        Observable<String> nameObservable = Observable.fromArray(names);
+        nameObservable.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull String name) {
+                Log.d(RxJavaApp.TAG, "Rx : \"" + name + "\"" + " has " + name.length() + " characters.");
+                tvText.setText(tvText.getText() + "Rx : \"" + name + "\"" + " has " + name.length() + " characters.\n");
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+}
